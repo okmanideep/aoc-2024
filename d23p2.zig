f4af0e8c230c2da8ec16ccc3fb8321786c9d5d21
Manideep Polireddi <okmanideep@users.noreply.github.com>
2025-04-12 11:17:42 -0030

    day23: optimized to ~100ms. YAY!
    Surprisingly 5s -> 100ms happened just because I am representing the
    node name as u16 instead of []u8 and connections per node are a set from
    get go
    
    There is one other massive unlock which is processing in the order of
    degeneracy -> process the lowest connected nodes first which will
    rapidly increase the `without` set and reduce the time spent while
    processing rapidly connected nodes - [Bron-Kerbosch algo for clique
    finding](https://en.wikipedia.org/wiki/Bron%E2%80%93Kerbosch_algorithm)

--- d23p2.zig
+++ d23p2.zig
@@ -3,15 +3,16 @@ const Allocator = std.mem.Allocator;
 const ArenaAllocator = std.heap.ArenaAllocator;
 
 const ByteList = std.ArrayList(u8);
+const IntList = std.ArrayList(u16);
 
 const Node = struct {
-    name: []const u8,
-    connections: *ByteList,
+    name: u16,
+    connections: *IntSet,
     allocator: Allocator,
 
-    fn init(allocator: Allocator, name: []const u8) !Node {
-        const connections_ptr = try allocator.create(ByteList);
-        connections_ptr.* = ByteList.init(allocator);
+    fn init(allocator: Allocator, name: u16) !Node {
+        const connections_ptr = try allocator.create(IntSet);
+        connections_ptr.* = try IntSet.init(allocator);
 
         return Node{ .name = name, .connections = connections_ptr, .allocator = allocator };
     }
@@ -21,130 +22,189 @@ const Node = struct {
         self.allocator.destroy(self.connections);
     }
 
-    fn addConnection(self: *Node, other_node_name: []const u8) !void {
-        std.debug.assert(other_node_name.len == 2);
-        try self.connections.appendSlice(other_node_name);
+    fn addConnection(self: *Node, other_node_name: u16) !void {
+        try self.connections.append(other_node_name);
     }
 
-    fn isConnectedTo(self: *Node, other_node_name: []const u8) bool {
-        return isPresentInConns(self.connections.items, other_node_name);
+    fn connectionsAsString(self: *Node, out: *ByteList) !void {
+        for (self.connections.values()) |item| {
+            var bytes: [2]u8 = undefined;
+            std.mem.writeInt(u16, &bytes, item, .big);
+            try out.appendSlice(bytes[0..]);
+        }
     }
 };
 
-const NodeMap = std.StringArrayHashMap(*Node);
+const IntVoidMap = std.AutoArrayHashMap(u16, void);
+const IntersectionOptions = struct {
+    out: *IntSet,
+    without: ?*const IntSet,
+};
+const IntSet = struct {
+    backing_map: *IntVoidMap,
+    allocator: Allocator,
 
-fn isPresentInConns(conns: []const u8, name: []const u8) bool {
-    std.debug.assert(name.len == 2);
-    std.debug.assert(conns.len % 2 == 0);
-    const num_conns = conns.len / 2;
-    for (0..num_conns) |i| {
-        const conn_name = conns[i * 2 .. i * 2 + 2];
-        if (std.mem.eql(u8, conn_name, name)) return true;
+    fn init(allocator: Allocator) !IntSet {
+        const map_ptr = try allocator.create(IntVoidMap);
+        map_ptr.* = IntVoidMap.init(allocator);
+
+        return IntSet{ .backing_map = map_ptr, .allocator = allocator };
     }
 
-    return false;
-}
+    fn initWithList(allocator: Allocator, list: *IntList) !IntSet {
+        var set = try IntSet.init(allocator);
+        try set.backing_map.ensureTotalCapacity(list.items.len);
 
-const ConnSet = std.StringHashMap(void);
-fn buildSet(set: *ConnSet, conns: []const u8) !void {
-    const count = conns.len / 2;
-    for (0..count) |i| {
-        try set.put(conns[2 * i .. 2 * i + 2], {});
-    }
-}
+        for (list.items) |item| {
+            try set.append(item);
+        }
 
-fn commonConnections(arena: *ArenaAllocator, first_conns: []const u8, second_conns: []const u8) ![]const u8 {
-    const allocator = arena.allocator();
-    var out = ByteList.init(allocator);
-    var set: ConnSet = ConnSet.init(arena.child_allocator);
-    defer set.deinit();
-
-    var conns = first_conns;
-    if (first_conns.len > second_conns.len) {
-        const size: u32 = @intCast(second_conns.len / 2);
-        try set.ensureTotalCapacity(size);
-        try buildSet(&set, second_conns);
-        conns = first_conns;
-    } else {
-        conns = second_conns;
-        const size: u32 = @intCast(first_conns.len / 2);
-        try set.ensureTotalCapacity(size);
-        try buildSet(&set, first_conns);
+        return set;
     }
 
-    const count = conns.len / 2;
-    for (0..count) |i| {
-        const conn_name = conns[i * 2 .. i * 2 + 2];
-        if (set.contains(conn_name)) {
-            try out.appendSlice(conn_name);
+    fn cloneWithAllocator(self: *const IntSet, allocator: Allocator) !IntSet {
+        const map_ptr = try allocator.create(IntVoidMap);
+        map_ptr.* = try self.backing_map.cloneWithAllocator(allocator);
+
+        return IntSet{ .backing_map = map_ptr, .allocator = allocator };
+    }
+
+    fn deinit(self: *IntSet) void {
+        self.backing_map.deinit();
+        self.allocator.destroy(self.backing_map);
+    }
+
+    fn values(self: *const IntSet) []u16 {
+        return self.backing_map.keys();
+    }
+
+    fn len(self: *const IntSet) usize {
+        return self.backing_map.keys().len;
+    }
+
+    fn intersection(self: *const IntSet, other: *const IntSet, options: IntersectionOptions) !void {
+        var list = self;
+        var set = other;
+
+        if (other.len() < self.len()) {
+            list = other;
+            set = self;
+        }
+
+        const out = options.out;
+        const without = options.without;
+
+        for (list.values()) |value| {
+            if (set.contains(value)) {
+                if (without) |wo| {
+                    if (!wo.contains(value)) {
+                        try out.append(value);
+                    }
+                } else {
+                    try out.append(value);
+                }
+            }
         }
     }
 
-    return out.items;
-}
+    fn contains(self: *const IntSet, value: u16) bool {
+        return self.backing_map.contains(value);
+    }
 
-fn isLess(left: []const u8, right: []const u8) bool {
-    if (left[0] > right[0]) return false;
-    if (left[0] < right[0]) return true;
+    fn append(self: *IntSet, value: u16) !void {
+        try self.backing_map.put(value, {});
+    }
+};
 
-    return left[1] < right[1];
-}
+const SortedIntList = struct {
+    backing_list: *IntList,
+    allocator: Allocator,
 
-fn binarySearch(sorted_conns: []const u8, name: []const u8) usize {
-    const conns_count = sorted_conns.len / 2;
-    var start: usize = 0;
-    var end: usize = conns_count;
+    fn init(allocator: Allocator) !SortedIntList {
+        const list_ptr = try allocator.create(IntList);
+        list_ptr.* = IntList.init(allocator);
+        std.debug.assert(list_ptr.items.len == 0);
+        return SortedIntList{ .backing_list = list_ptr, .allocator = allocator };
+    }
 
-    while (end - start > 1) {
-        const mid = start + ((end - start) / 2);
-        const conn_name = sorted_conns[2 * mid .. 2 * mid + 2];
-        if (isLess(conn_name, name)) {
-            start = mid;
+    fn initSingle(allocator: Allocator, value: u16) !SortedIntList {
+        const list_ptr = try allocator.create(IntList);
+        list_ptr.* = try IntList.initCapacity(allocator, 1);
+        try list_ptr.append(value);
+
+        std.debug.assert(list_ptr.items.len == 1);
+        return SortedIntList{ .backing_list = list_ptr, .allocator = allocator };
+    }
+
+    fn deinit(self: *SortedIntList) void {
+        self.backing_list.deinit();
+        self.allocator.destroy(self.backing_list);
+    }
+
+    fn copyFrom(self: *SortedIntList, other: *const SortedIntList) !void {
+        try self.backing_list.appendSlice(other.backing_list.items);
+    }
+
+    fn cloneWithAllocator(self: *const SortedIntList, allocator: Allocator) !SortedIntList {
+        const list_ptr = try allocator.create(IntList);
+        list_ptr.* = try IntList.initCapacity(allocator, self.backing_list.items.len);
+        list_ptr.appendSliceAssumeCapacity(self.backing_list.items);
+
+        return SortedIntList{ .backing_list = list_ptr, .allocator = allocator };
+    }
+
+    fn len(self: *const SortedIntList) usize {
+        return self.backing_list.items.len;
+    }
+
+    fn at(self: *const SortedIntList, index: usize) u16 {
+        return self.backing_list.items[index];
+    }
+
+    fn indexToAddAt(self: *const SortedIntList, value: u16) usize {
+        var start: usize = 0;
+        var end: usize = self.backing_list.items.len;
+
+        while (end > start + 1) {
+            const mid = start + ((end - start) / 2);
+            if (self.at(mid) < value) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+
+        if (self.at(start) < value) {
+            return start + 1;
         } else {
-            end = mid;
+            return start;
         }
     }
 
-    const conn_name = sorted_conns[2 * start .. 2 * start + 2];
-    if (isLess(conn_name, name)) {
-        return start + 1;
-    } else {
-        return start;
+    fn append(self: *SortedIntList, value: u16) !void {
+        const index_to_add = self.indexToAddAt(value);
+        try self.backing_list.insert(index_to_add, value);
     }
-}
 
-fn insertInConns(arena: *ArenaAllocator, sorted_conns: []const u8, name: []const u8) ![]const u8 {
-    const allocator = arena.allocator();
-    var out = try ByteList.initCapacity(allocator, sorted_conns.len + name.len);
+    fn values(self: *const SortedIntList) []const u16 {
+        return self.backing_list.items;
+    }
 
-    const index = binarySearch(sorted_conns, name);
-    try out.appendSlice(sorted_conns[0 .. 2 * index]);
-    try out.appendSlice(name);
-    try out.appendSlice(sorted_conns[2 * index ..]);
+    fn asString(self: *const SortedIntList, out: *ByteList) !void {
+        const length = self.backing_list.items.len;
+        for (self.backing_list.items, 0..) |item, i| {
+            var bytes: [2]u8 = undefined;
+            std.mem.writeInt(u16, &bytes, item, .big);
+            try out.appendSlice(bytes[0..]);
 
-    return out.items;
-}
-
-fn debugPrintConns(conns: []const u8) void {
-    std.debug.assert(conns.len % 2 == 0);
-
-    const count = conns.len / 2;
-    for (0..count) |i| {
-        std.debug.print("{s}", .{conns[2 * i .. 2 * i + 2]});
-        if (i < count - 1) {
-            std.debug.print(",", .{});
+            if (i < length - 1) {
+                try out.append(',');
+            }
         }
     }
+};
 
-    std.debug.print("\n", .{});
-}
-
-fn max(first: usize, second: usize) usize {
-    if (first > second) return first;
-    return second;
-}
-
-const LargestPartyCache = std.StringHashMap([]const u8);
+const NodeMap = std.AutoArrayHashMap(u16, *Node);
 
 const Graph = struct {
     node_map: *NodeMap,
@@ -156,8 +216,11 @@ const Graph = struct {
 
         var tokenizer = std.mem.tokenizeScalar(u8, network, '\n');
         while (tokenizer.next()) |conn_as_string| {
-            const first_node_name = conn_as_string[0..2];
-            const second_node_name = conn_as_string[3..5];
+            const first_node_name_as_string = conn_as_string[0..2];
+            const second_node_name_as_string = conn_as_string[3..5];
+
+            const first_node_name = std.mem.readInt(u16, first_node_name_as_string, .big);
+            const second_node_name = std.mem.readInt(u16, second_node_name_as_string, .big);
 
             if (map_ptr.get(first_node_name)) |first_node_ptr| {
                 try first_node_ptr.addConnection(second_node_name);
@@ -193,82 +256,72 @@ const Graph = struct {
         self.allocator.destroy(self.node_map);
     }
 
-    fn getLanPartyCount(self: *Graph) usize {
-        var count: usize = 0;
-
-        const keys = self.node_map.keys();
-        for (0..keys.len) |i| {
-            const first_conn_name = keys[i];
-            const first_conn_node = self.node_map.get(first_conn_name) orelse unreachable;
-            for (i + 1..keys.len) |j| {
-                const second_conn_name = keys[j];
-                if (!first_conn_node.isConnectedTo(second_conn_name)) continue;
-
-                const second_conn_node = self.node_map.get(second_conn_name) orelse unreachable;
-
-                for (j + 1..keys.len) |k| {
-                    const third_conn_name = keys[k];
-                    if (first_conn_node.isConnectedTo(third_conn_name) and second_conn_node.isConnectedTo(third_conn_name)) {
-                        if (first_conn_name[0] == 't' or second_conn_name[0] == 't' or third_conn_name[0] == 't') {
-                            count += 1;
-                        }
-                    }
-                }
-            }
-        }
-
-        return count;
+    fn connectionsAsString(self: *Graph, out: *ByteList, name_as_string: *const [2]u8) !void {
+        const name = std.mem.readInt(u16, name_as_string, .big);
+        const node = self.node_map.get(name) orelse unreachable;
+        try node.connectionsAsString(out);
     }
 
-    fn largestLanPartyWith(self: *Graph, arena: *ArenaAllocator, cache: *LargestPartyCache, members: []const u8, potential_members: []const u8, min_conns: usize) ![]const u8 {
-        std.debug.assert(members.len % 2 == 0);
-        std.debug.assert(potential_members.len % 2 == 0);
-        if (potential_members.len == 0) return members;
-        if (cache.get(members)) |value| {
-            return value;
+    fn largestParty(self: *Graph, arena: *ArenaAllocator, out: *SortedIntList, members: *const SortedIntList, potential_members: *const IntSet, without: *const IntSet) !void {
+        if (potential_members.len() == 0) {
+            try out.copyFrom(members);
+            return;
         }
 
-        // std.debug.print("members: {s}, potential_members: {s}\n", .{members, potential_members});
-        if (potential_members.len == 2) return try insertInConns(arena, members, potential_members);
+        const allocator = arena.allocator();
+        var largest_party = try members.cloneWithAllocator(allocator);
 
-        var largest_party = members;
-        const potential_member_count = potential_members.len / 2;
-        for (0..potential_member_count) |i| {
-            const potential_member_name = potential_members[2 * i .. 2 * i + 2];
-            const potential_member = self.node_map.get(potential_member_name) orelse unreachable;
+        var completed = try without.cloneWithAllocator(allocator);
+        for (potential_members.values()) |item| {
+            var new_members = try members.cloneWithAllocator(allocator);
+            try new_members.append(item);
+            const node = self.node_map.get(item) orelse unreachable;
 
-            const remaining_potential_members = try commonConnections(arena, potential_members, potential_member.connections.items);
-            if ((members.len / 2) + (remaining_potential_members.len / 2) + 1 < min_conns) {
-                continue;
+            var remaining_potential_members = try IntSet.init(allocator);
+            try potential_members.intersection(node.connections, .{ .out = &remaining_potential_members, .without = &completed });
+
+            var party = try SortedIntList.init(allocator);
+
+            try self.largestParty(arena, &party, &new_members, &remaining_potential_members, &completed);
+            if (party.len() > largest_party.len()) {
+                largest_party = try party.cloneWithAllocator(allocator);
             }
-            const new_members = try insertInConns(arena, members, potential_member_name);
 
-            const party = try self.largestLanPartyWith(arena, cache, new_members, remaining_potential_members, max(min_conns, largest_party.len / 2));
-            if (party.len > largest_party.len) {
-                largest_party = party;
-            }
+            try completed.append(item);
         }
 
-        try cache.put(members, largest_party);
-
-        return largest_party;
+        try out.copyFrom(&largest_party);
     }
 
-    fn getLargestLanParty(self: *Graph, arena: *ArenaAllocator) ![]const u8 {
-        var largest_party: []const u8 = "";
-        var cache = LargestPartyCache.init(arena.allocator());
+    fn getLargestParty(self: *Graph, out: *SortedIntList) !void {
+        var largest_party = try SortedIntList.init(self.allocator);
+        var arena = ArenaAllocator.init(self.allocator);
+        defer arena.deinit();
 
+        const arena_allocator = arena.allocator();
+
+        var completed = try IntSet.init(self.allocator);
+        defer completed.deinit();
         var iterator = self.node_map.iterator();
         while (iterator.next()) |entry| {
             const name = entry.key_ptr.*;
             const node = entry.value_ptr.*;
-            const party = try self.largestLanPartyWith(arena, &cache, name, node.connections.items, largest_party.len / 2);
-            if (party.len > largest_party.len) {
-                largest_party = party;
+
+            const members = try SortedIntList.initSingle(arena_allocator, name);
+            var party = try SortedIntList.init(arena_allocator);
+            try self.largestParty(&arena, &party, &members, node.connections, &completed);
+            if (party.len() > largest_party.len()) {
+                largest_party.deinit();
+
+                largest_party = try party.cloneWithAllocator(self.allocator);
             }
+
+            try completed.append(name);
+            _ = arena.reset(.retain_capacity);
         }
 
-        return largest_party;
+        try out.copyFrom(&largest_party);
+        largest_party.deinit();
     }
 };
 
@@ -282,13 +335,15 @@ pub fn main() !void {
     var graph = try Graph.init(allocator, INPUT);
     defer graph.deinit();
 
-    std.debug.print("Number of nodes: {}\n", .{graph.node_map.keys().len});
+    var largest_party = try SortedIntList.init(allocator);
+    defer largest_party.deinit();
 
-    var arena = ArenaAllocator.init(allocator);
-    defer arena.deinit();
+    var out = ByteList.init(allocator);
+    defer out.deinit();
 
-    const largest_party = try graph.getLargestLanParty(&arena);
-    debugPrintConns(largest_party);
+    try graph.getLargestParty(&largest_party);
+    try largest_party.asString(&out);
+    std.debug.print("Result: {s}\n", .{out.items});
 }
 
 test "graph init" {
@@ -302,11 +357,29 @@ test "graph init" {
     var graph = try Graph.init(allocator, network);
     defer graph.deinit();
 
-    try std.testing.expectEqualSlices(u8, "tcqp", graph.node_map.get("kh").?.connections.items);
-    try std.testing.expectEqualSlices(u8, "kh", graph.node_map.get("tc").?.connections.items);
-    try std.testing.expectEqualSlices(u8, "kh", graph.node_map.get("qp").?.connections.items);
-    try std.testing.expectEqualSlices(u8, "cg", graph.node_map.get("de").?.connections.items);
-    try std.testing.expectEqualSlices(u8, "de", graph.node_map.get("cg").?.connections.items);
+    var out = ByteList.init(allocator);
+    defer out.deinit();
+
+    try graph.connectionsAsString(&out, "kh");
+    try std.testing.expectEqualSlices(u8, "tcqp", out.items);
+    out.clearRetainingCapacity();
+
+    try graph.connectionsAsString(&out, "tc");
+    try std.testing.expectEqualSlices(u8, "kh", out.items);
+    out.clearRetainingCapacity();
+
+    try graph.connectionsAsString(&out, "qp");
+    try std.testing.expectEqualSlices(u8, "kh", out.items);
+    out.clearRetainingCapacity();
+
+    try graph.connectionsAsString(&out, "de");
+    try std.testing.expectEqualSlices(u8, "cg", out.items);
+    out.clearRetainingCapacity();
+
+    try graph.connectionsAsString(&out, "cg");
+    try std.testing.expectEqualSlices(u8, "de", out.items);
+    out.clearRetainingCapacity();
+
     try std.testing.expectEqual(5, graph.node_map.keys().len);
 }
 
@@ -350,8 +423,13 @@ test "aoc example" {
     var graph = try Graph.init(allocator, network);
     defer graph.deinit();
 
-    var arena = ArenaAllocator.init(allocator);
-    defer arena.deinit();
+    var largest_party = try SortedIntList.init(allocator);
+    defer largest_party.deinit();
 
-    try std.testing.expectEqualSlices(u8, "codekata", try graph.getLargestLanParty(&arena));
+    var out = ByteList.init(allocator);
+    defer out.deinit();
+
+    try graph.getLargestParty(&largest_party);
+    try largest_party.asString(&out);
+    try std.testing.expectEqualSlices(u8, "co,de,ka,ta", out.items);
 }
